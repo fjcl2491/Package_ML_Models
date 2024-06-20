2dde7e4b52e52cbf1bd6b3ecedc229869481c2f4
Fran Castro <castro_59@hotmail.com>
2024-06-20 15:20:48 +0200

    config.py file ready

--- packaging_ml_model/prediction_model/config/config.py
+++ packaging_ml_model/prediction_model/config/config.py
@@ -17,13 +17,17 @@ SAVE_MODEL_PATH = os.path.join(PACKAGE_ROOT, "trained_models")
 TARGET = "Loan_Status"
 
 # Final features used in the model to predict the target
-FEATURES = []
+FEATURES = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'ApplicantIncome', 'LoanAmount',
+            'Loan_Amount_Term', 'Credit_History','Property_Area']
 
 # Numeric features
-NUM_FEATURES = []
+NUM_FEATURES = ['ApplicantIncome', 'LoanAmount', 'Loan_Amount_Term']
 
 # Categorical features
-CAT_FEATURES = []
+CAT_FEATURES = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed','Credit_History','Property_Area']
 
 # Features to encode
-FEATURES_TO_ENCODE = []
+FEATURES_TO_ENCODE = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed','Credit_History','Property_Area']
+
+FEATURE_TO_MODIFY = ['ApplicantIncome']
+FEATURE_TO_ADD = ['CoApplicantIncome']
